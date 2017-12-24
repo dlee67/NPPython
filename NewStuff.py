94afa1d9203854f2f09e1044681fed4742b0a74e
bob <dlee67@msudenver.edu>
2017-12-24 16:57:47 -0700

    Christmas, and stuff...

--- NewStuff.py
+++ NewStuff.py
@@ -2,7 +2,12 @@ import urllib.request
 
 rfc_number = input("Enter the RFC number: ");
 
-byte_stream = urllib.request.urlopen("http://www.ietf.org/rfc/rfc" + rfc_number + ".txt").read()
-decoded_stream = byte_stream.decode()
-
+try:
+	byte_stream = urllib.request.urlopen("http://www.ietf.org/rfc/rfc" + rfc_number + ".txt").read()
+	decoded_stream = byte_stream.decode()
+except urllib.error.HTTPError as e:
+	print("Status code: ", e.code)
+	print("Reason: ", e.reason)
+	print("URL: ", e.url)
+	
 print(decoded_stream)
